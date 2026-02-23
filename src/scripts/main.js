d0708c18aca65ba329940603265c94b67856ccb9
uselessmorning <shtoykoigor@ukr.net>
2026-02-23 16:03:25 +0100

    soluiton

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write code here
+
+const toNum = (str) => Number(str.replace(/[$,]/g, ''));
+
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = toNum(a.dataset.salary);
+    const salaryB = toNum(b.dataset.salary);
+
+    const validA = Number.isFinite(salaryA) ? salaryA : 0;
+    const validB = Number.isFinite(salaryB) ? salaryB : 0;
+
+    return validB - validA;
+  });
+
+  list.append(...items);
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.children);
+
+  return items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      salary: toNum(item.dataset.salary),
+    };
+  });
+}
+
+const employeesList = document.querySelector('ul');
+
+sortList(employeesList);
+getEmployees(employeesList);
